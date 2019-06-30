89f166805e8f323b388df9894eeda20aeddb1a21
chee yang <tehcheeyang@gmail.com>
2019-07-01 00:02:40 +0800

    styling for main container

--- src/App.js
+++ src/App.js
@@ -7,22 +7,41 @@ import AppFooter from "./containers/AppFooter";
 import AppContent from "./containers/AppContent";
 import { connect } from "react-redux";
 import { select } from "./store";
+import Drawer from "@material-ui/core/Drawer";
 
 const styles = {
   appRoot: {
-    height: "100vh"
+    height: "100vh",
+    flexGrow: 2
+  },
+  appDrawer: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    minWidth: "20vw"
   }
 };
 
 const App = props => {
   const { classes, user } = props;
   return (
-    <Grid container direction="column" className={classes.appRoot}>
-      {/* TODO: Set up navbar within header for navigation within menu item clicked in drawer*/}
-      <AppHeader hidden={!user} />
-      <AppContent />
-      <AppFooter hidden={!user} />
-      {/* TODO: Set up Drawer Component for navigation to Portfolio, Trade, */}
+    <Grid container direction="row">
+      <Drawer
+        variant="persistent"
+        anchor="left"
+        className={classes.appDrawer}
+        open={true}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      />
+      <Grid container direction="column" className={classes.appRoot}>
+        {/* TODO: Set up navbar within header for navigation within menu item clicked in drawer*/}
+        <AppHeader hidden={!user} />
+        <AppContent />
+        <AppFooter hidden={!user} />
+        {/* TODO: Set up Drawer Component for navigation to Portfolio, Trade, */}
+      </Grid>
     </Grid>
   );
 };
