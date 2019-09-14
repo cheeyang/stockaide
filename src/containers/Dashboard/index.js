3a7553324a0fcf53d0d8ce7eb634efaecf7f6850
chee yang <tehcheeyang@gmail.com>
2019-09-14 14:07:27 +0800

    enabled toggle theme

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -5,9 +5,18 @@ import Button from "@material-ui/core/Button";
 import { fetchSimFinIdByTicker } from "../../api/SimFin";
 import { useDispatch, useSelector } from "react-redux";
 import { THEMES } from "../../constants";
+import { select } from "../../store";
 
 const Dashboard = props => {
   const dispatch = useDispatch();
+  const themeName = useSelector(select.app.getTheme);
+  const handleChangeTheme = () => {
+    if (themeName === THEMES.DARK) {
+      dispatch.app.setTheme(THEMES.LIGHT);
+    } else {
+      dispatch.app.setTheme(THEMES.DARK);
+    }
+  };
 
   return (
     <Grid container justify="center" alignItems="center" direction="column">
@@ -15,9 +24,7 @@ const Dashboard = props => {
       <Button onClick={() => fetchSimFinIdByTicker("gpro")}>Hit API</Button>
       <br />
       <br />
-      <Button onClick={() => dispatch.app.setTheme(THEMES.DARK)}>
-        Change Theme
-      </Button>
+      <Button onClick={handleChangeTheme}>Change Theme</Button>
     </Grid>
   );
 };
